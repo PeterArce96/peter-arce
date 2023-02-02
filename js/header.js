8a76334e08c7a683a6450cc5a0a6468edeb22ff3
PeterArce96 <parce0496@gmail.com>
2023-02-02 17:22:33 -0500

    portafolio - cambiando colores, tipo de letra - profile y header

--- js/header.js
+++ js/header.js
@@ -1,6 +1,4 @@
 const header = () => {
-    const header = document.querySelector('.header');
-    const headerNav = document.querySelector('.header-nav');
 
     const headerNavMenuIcon = document.getElementById('headerNavMenuIcon');
     const headerMenuCloseIcon = document.getElementById('headerMenuCloseIcon');
@@ -8,10 +6,6 @@ const header = () => {
     const headerNavItems = document.querySelectorAll('.header-nav__item');
 
 
-    const documentScroll = () =>{
-        header.classList.toggle('header--scroll', window.scrollY > 0);
-        headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0)
-    }
 
     const openMenu = () => {
         headerNavMenuLinkList.classList.add('header-nav__menu-link-list--open');
@@ -20,7 +14,6 @@ const header = () => {
         headerNavMenuLinkList.classList.remove('header-nav__menu-link-list--open');
     };
 
-    document.addEventListener('scroll', documentScroll);
     headerNavMenuIcon.addEventListener('click', openMenu);
     headerMenuCloseIcon.addEventListener('click', closeMenu);
     headerNavItems.forEach((element) =>{
